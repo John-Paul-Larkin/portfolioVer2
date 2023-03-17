acf3690fa0660106433f9f5b1bb1891218092241
john Paul Larkin <johnplarkin@gmail.com>
2023-03-17 20:50:19 +0000

    icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import "./App.css";
 import ContactForm from "./Components/ContactForm/ContactForm";
 import Footer from "./Components/Footer/Footer";
 import HeroScreen from "./Components/Hero/HeroScreen";
-import Icons from "./Components/Icons/DevIcons";
+import DevIcons from "./Components/Icons/DevIcons";
 import Projects from "./Components/Projects/Projects";
 
 function App() {
   return (
     <div>
       <HeroScreen />
-
       <Projects />
-      <Icons />
-
+      <DevIcons />
       <ContactForm />
       <Footer />
     </div>
--- src/Components/Icons/DevIcons.tsx
+++ src/Components/Icons/DevIcons.tsx
@@ -1,72 +1,76 @@
-import { useAnimation, useInView } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
 import "./DevIcons.css";
 
 export default function DevIcons() {
-  const controls = useAnimation();
-  const animateRef = useRef(null);
-  const isInView = useInView(animateRef);
-
-  const animation = {
-    initial: { opacity: 0, y: 10 },
-    animate: { opacity: 1, y: 0, transition: { delay: 0.2, duration: 0.25, ease: "easeOut" } },
+  const list = {
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.05,
+      },
+    },
+    hidden: {
+      opacity: 1,
+      transition: {
+        when: "afterChildren",
+      },
+    },
   };
 
-  useEffect(() => {
-    if (isInView) {
-      controls.start(animation.animate);
-    } else {
-      controls.start(animation.initial);
-    }
-  }, [controls, isInView, animation.animate, animation.initial]);
+  const item = {
+    visible: { opacity: 1, y: 0 },
+    hidden: {
+      opacity: 0,
+      y: 50,
+      transition: {
+        delay: 1,
+        ease: "easeOut",
+      },
+    },
+  };
 
   return (
-    <div className="dev-icons" ref={animateRef}>
-      <ul className="container">
-        <li>
+    <div className="dev-icons">
+      <motion.ul className="container" initial="hidden" whileInView="visible" variants={list}>
+        <motion.li variants={item}>
           <i className="devicon-firebase-plain colored"></i>
           <span>Firebase</span>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={item}>
           <i className="devicon-react-original colored"></i>
           <span>React</span>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={item}>
           <i className="devicon-typescript-plain colored"></i>
           <span>Typescript</span>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={item}>
           <i className="devicon-redux-original colored"></i>
           <span>Redux</span>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={item}>
           <i className="devicon-html5-plain colored"></i>
           <span>HTML</span>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={item}>
           <i className="devicon-vscode-plain colored"></i>
           <span>VSCode</span>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={item}>
           <i className="devicon-github-original colored"></i>
           <span>Github</span>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={item}>
           <i className="devicon-git-plain colored"></i>
           <span>Git</span>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={item}>
           <i className="devicon-javascript-plain colored"></i>
           <span>Javascript</span>
-        </li>
-      </ul>
+        </motion.li>
+      </motion.ul>
     </div>
   );
 }
 
-{
-  /* <i className="devicon-firebase-plain-wordmark colored"></i>*/
-}
-{
-  /* <i className="devicon-react-original-wordmark"></i> */
-}
