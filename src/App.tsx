0aca04c9ff946de946a35e6d4a02a705094d7ca0
john Paul Larkin <johnplarkin@gmail.com>
2023-04-12 23:32:57 +0100

    initials anim

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import "./App.css";
-import ContactForm from "./Components/ContactForm/ContactForm";
 import Footer from "./Components/Footer/Footer";
 import DevIcons from "./Components/Icons/DevIcons";
-import Projects from "./Components/Projects/MainProject/Projects";
 // import "../src/Components/Hero/HeroTest.css";
 import HeroScreenTest from "./Components/Hero/HeroScreenTest";
 // import Test from "./Components/Test/Test";
-import React from "react";
+import React, { Suspense, lazy } from "react";
 // import Test from "./Components/Test/Test";
 import PortfolioStore from "./Context/PortfolioContext";
-// import HeroScreenTest2 from "./Components/Hero/HeroScreenTest2";
+import HeroScreenTest2 from "./Components/Hero/HeroScreenTest2";
 // import HeroScreen from "./Components/Hero/HeroScreen";
+
+import Projects from "./Components/Projects/MainProject/Projects";
+import ContactForm from "./Components/ContactForm/ContactForm";
+
+// const ContactForm = lazy(() => import("./Components/ContactForm/ContactForm"));
 
 function App() {
   return (
@@ -18,10 +21,12 @@ function App() {
       <PortfolioStore>
         {/* <Test /> */}
         {/* <HeroScreen/> */}
-        <HeroScreenTest />
+        <HeroScreenTest2 />
         <Projects />
         <DevIcons />
-        <ContactForm />
+        {/* <Suspense> */}
+          <ContactForm />
+        {/* </Suspense> */}
         <Footer />
       </PortfolioStore>
     </>
